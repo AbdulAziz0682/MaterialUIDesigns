83e71c7d8888d55325b07f4eea612c440ee8fcf0
AbdulAziz0682 <abdulrasheed2132@gmail.com>
2021-09-01 07:44:16 +0500

    Installed Material UI

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+export default function App(){
+    return (
+        <Typography variant="h1">This is React app setup with Parcel Bundler</Typography>
+    )
+}
